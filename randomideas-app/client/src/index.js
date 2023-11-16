16a539e1c4cd50d936d901e2d1a4f48ad0d7b7b3
prem <premkumardanav@gmail.com>
2023-11-16 06:54:15 +0530

    added

---
+++ randomideas-app/client/src/index.js
@@ -0,0 +1,10 @@
+//import '@fortawesome/fontawesome-free/css/all.css';
+import IdeaForm from './components/IdeaForm';
+import IdeaList from './components/IdeaList';
+import Modal from './components/Modal';
+import './css/style.css';
+
+new Modal();
+const ideaForm = new IdeaForm();
+ideaForm.render();
+new IdeaList();
